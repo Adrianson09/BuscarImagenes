f6a23a414f33851ebfaf23440bbd2f84be5df4bb
Adrian Guerrero <adrianson@outlook.com>
2023-07-13 20:32:46 -0600

    Iniciando Paginacion

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const registrosPorPagina = 40;
+
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario);
@@ -42,7 +44,7 @@ function mostrarAlerta(mensaje){
 
 function buscarImagenes(termino){
     const key ='38238248-ae984a2fad41f6c4f55a7873f';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
 
     fetch(url)
     .then(respuesta => respuesta.json())
@@ -51,10 +53,34 @@ function buscarImagenes(termino){
     })
 
 }
+function calcularPaginas(total){
+    return parseInt( Math.ceil(total / registrosPorPagina ));
+}
+
 function mostrarImagenes(imagenes){
   
     while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild)
     }
-    
+    // Iterar Imagenes y hacer HTML
+    imagenes.forEach(imagen => {
+        const { id, largeImageURL, previewURL} = imagen;
+
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4 ">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+                <div class="p-4">
+                    <a class="block w-full bg-blue-800 hover:bg-blue-700 text-white uppercase text-center font-bold rounded
+                    mt-1 p-1" 
+                    href="${largeImageURL}" 
+                    target="_blank" rel="noopener noreferrer">
+                    Ver Imagen
+                    </a>
+                </div>
+            </div>
+        </div>
+        `
+    });
+
 }
